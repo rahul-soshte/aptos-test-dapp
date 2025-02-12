1ae449a22e9a69b795cb8fd4aafc7949a9ad96e6
rahul-soshte <rahul.soshte4771@gmail.com>
2025-02-12 09:18:28 +0000

    pulling data onchain

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
+import { Aptos, AptosConfig, Network } from "@aptos-labs/ts-sdk";
+import { Layout, Row, Col, Button } from 'antd';
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
 
-import { Layout, Row, Col } from 'antd';
+// const aptosConfig = new AptosConfig({ network: Network.MAINNET });
+// const aptos = new Aptos(aptosConfig);
+const aptos = new Aptos();
+
+
 
 function App() {
+  const { account } = useWallet();
+  const [accountHasList, setAccountHasList] = useState<boolean>(false);
+
+  const fetchList = async () => {
+    if (!account) return [];
+    // change this to be your module account address
+    const moduleAddress = "1ac991fa18c504de4b14d3391053a0f114280c4efc1b1973170b14bb5d08b27d";
+    try {
+      const todoListResource = await aptos.getAccountResource(
+        {
+          accountAddress:account?.address,
+          resourceType:`${moduleAddress}::todolist::TodoList`
+        }
+      );
+      setAccountHasList(true);
+    } catch (e: any) {
+      setAccountHasList(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, [account?.address]);
+  
   return (
     <>
       <Layout>
         <Row align="middle">
           <Col span={10} offset={2}>
-            <h1>Our Todolist</h1>
+            <h1>Our todolist</h1>
           </Col>
           <Col span={12} style={{ textAlign: "right", paddingRight: "200px" }}>
             <WalletSelector />
           </Col>
         </Row>
       </Layout>
+      {!accountHasList && (
+        <Row gutter={[0, 32]} style={{ marginTop: "2rem" }}>
+          <Col span={8} offset={8}>
+            <Button
+              block
+              type="primary"
+              style={{ height: "40px", backgroundColor: "#3f67ff" }}
+            >
+              Add new list
+            </Button>
+          </Col>
+        </Row>
+      )}
     </>
   );
 }
